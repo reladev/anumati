9cbd5aa1a6b542d3a91239705d6d6099c7f10245
jkleiss <jeffg.npc@gmail.com>
2018-02-19 20:09:42 -0500

    Removed Asset

--- hibernate_test/src/main/java/org/reladev/anumati/hibernate_test/security/SecurityObjectType.java
+++ hibernate_test/src/main/java/org/reladev/anumati/hibernate_test/security/SecurityObjectType.java
@@ -2,7 +2,6 @@ package org.reladev.anumati.hibernate_test.security;
 
 import org.reladev.anumati.SecuredByRef;
 import org.reladev.anumati.SecuredObjectType;
-import org.reladev.anumati.hibernate_test.entity.Asset;
 import org.reladev.anumati.hibernate_test.entity.Company;
 import org.reladev.anumati.hibernate_test.entity.Project;
 import org.reladev.anumati.hibernate_test.entity.Ticket;
@@ -12,15 +11,12 @@ public enum SecurityObjectType implements SecuredObjectType {
 	COMPANY(Company.class),
     PROJECT(Project.class),
     USER(User.class),
-    TICKET(Ticket.class),
-	ASSET(Asset.class);
+    TICKET(Ticket.class);
 
 	public static final int COMPANY_ORDINAL = 0;
     public static final int PROJECT_ORDINAL = 1;
     public static final int USER_ORDINAL = 2;
     public static final int TICKET_ORDINAL = 3;
-    public static final int SUBMISSION_ORDINAL = 4;
-	public static final int ASSET_ORDINAL = 5;
 
 	private Class<? extends SecuredByRef> typeClass;
 
